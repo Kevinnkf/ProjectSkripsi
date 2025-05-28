d2efad1e1e8e8a6bb5854495364a9a6e75e0c221
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-05-28 15:29:59 +0700

    update: baseURL

--- FrontEnd/feskripsi/src/services/axios.js
+++ FrontEnd/feskripsi/src/services/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BE_API,
+  baseURL: 'https://be-service-production.up.railway.app/api',
   withCredentials: true,
 });
 
