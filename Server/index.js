63b96150babe9078e5555c8f7b332c671d3f5e28
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-05-29 07:52:23 +0700

    add: origin cors, localhost:5173

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 
 // Allow your frontend to talk to this API
 app.use(cors({
-  origin:      process.env.FRONTEND_URL || 'https://halo-pnj.vercel.app',
+  origin:      process.env.FRONTEND_URL || 'https://halo-pnj.vercel.app' || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Authorization', 'Content-Type']
