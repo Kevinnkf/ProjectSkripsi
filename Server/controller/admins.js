330f7ae851a0815717046a7d4af9548b9a0c354d
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-05-29 07:51:26 +0700

    edit: secure to true

--- Server/controller/admins.js
+++ Server/controller/admins.js
@@ -40,8 +40,8 @@ export async function login(req, res) {
     // Cookie options
     const cookieOptions = {
       maxAge: 20 * 60 * 1000,
-      httpOnly: false,
-      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      secure: true,
       sameSite: 'None'
     };
 
