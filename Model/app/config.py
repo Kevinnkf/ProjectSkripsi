3b1601032e69aa5598e4e002f7b359f1997ffa20
ghaniashafiqa <ghaniashafiqaa@gmail.com>
2025-06-21 15:15:18 +0700

    change: collection name

--- Model/app/config.py
+++ Model/app/config.py
@@ -5,6 +5,6 @@ load_dotenv(dotenv_path=".env")
 
 QDRANT_URL      = os.getenv("QDRANT_URL")
 QDRANT_API_KEY  = os.getenv("QDRANT_API_KEY")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME", "rag-academics-collection-small")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "rag-academics-collection-large")
 
 BACKEND_URL = os.getenv("BACKEND_URL")
